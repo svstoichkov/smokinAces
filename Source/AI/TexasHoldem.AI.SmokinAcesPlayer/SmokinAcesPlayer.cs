1c458c40f7415bd015a8c7c6511af25884df10cb
svstoichkov <sv.stoichkov@yahoo.com>
2015-11-11 18:16:23 +0200

    Added SmokinAcesPlayer project

---
+++ Source/AI/TexasHoldem.AI.SmokinAcesPlayer/SmokinAcesPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem.AI.SmokinAcesPlayer
+{
+    public class Class1
+    {
+    }
+}
